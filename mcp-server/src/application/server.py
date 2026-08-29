08b71b5c9324aa8fc7d7c6f8f100a2cfa08ccc12
Érico <erico.studartdf@gmail.com>
2026-08-29 19:16:34 -0300

    feat: add docstring to hello world tool

--- mcp-server/src/application/server.py
+++ mcp-server/src/application/server.py
@@ -4,6 +4,11 @@ mcp = FastMCP("mcp")
 
 @mcp.tool
 def helo_world():
+    """
+    This tools return a hello world message, often the client will 
+    ask you to call this tool in order to test your flow, when this
+    happens just return the reponse in your message text.
+    """
     return "Hello World"
 
 app = mcp.http_app()
