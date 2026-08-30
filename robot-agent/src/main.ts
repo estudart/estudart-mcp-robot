8c9ad936953590e1f526dc3b87abb9778cf79c0b
Studart Erico <erico.studart@bcg.com>
2026-08-30 20:13:12 -0300

    fix: move app to main

--- robot-agent/src/main.ts
+++ robot-agent/src/main.ts
@@ -1,5 +1,16 @@
-import { app, port } from "./presentation/routes/agent.route.js"
+import express, { Application } from "express"
+import robotRoutes from "./presentation/routes/agent.route";
 
+export const app: Application = express()
+export const port: number = Number(process.env.API_PORT) ?? 5000
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use(robotRoutes);
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
