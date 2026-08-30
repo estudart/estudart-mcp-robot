931e8b9cb4ec5f8e559438f979ae827341f15d2a
Érico <erico.studartdf@gmail.com>
2026-08-30 11:29:52 -0300

    feat: add motors to adapter

--- mcp-server/src/infrastructure/robot_engine_adapter.py
+++ mcp-server/src/infrastructure/robot_engine_adapter.py
@@ -6,5 +6,19 @@ class RobotAdapter:
     def __init__(self):
         self._robot = Robot()
 
+    ## LEDS
     def set_all_leds(self, color: LedColor):
         self._robot.leds.set_all(color=color)
+    
+    # MOTOR
+    def move_forward(self, speed: int):
+        self._bot.motors.forward(speed)
+
+    def move_backwards(self, speed: int):
+        self._bot.motors.backwards(speed)
+    
+    def turn_left(self, speed: int):
+        self._bot.motors.turn_left(speed)
+
+    def turn_right(self, speed: int):
+        self._bot.motors.turn_right(speed)
