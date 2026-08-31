74864fc3612bea6636c94b24e7daf03bbdefaac1
Studart Erico <erico.studart@bcg.com>
2026-08-31 13:39:30 -0300

    feat: connect with wss server

--- robot-agent-fe/src/pages/RobotChat.tsx
+++ robot-agent-fe/src/pages/RobotChat.tsx
@@ -1,8 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./RobotChat.module.css"
 
 export default function Chat() {
     const [message, setMessage] = useState("");
+    const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+    const [history, setHistory] = useState<String[]>([]);
+
+    useEffect(() => {
+        const ws = new WebSocket("ws://localhost:8080");
+
+        ws.onopen = () => {
+            console.log("Websocket connection opened");
+            setWebSocket(ws);
+        }
+
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (data.type === "response") {
+                setHistory(prev => [...prev, data.message])
+            }
+        };
+
+        ws.onclose = () => {
+            console.log("Connection was closed");
+            setWebSocket(null);
+        }
+
+        return () => {
+            if (ws) {
+                ws.close();
+                setWebSocket(null);
+            }
+        };
+    }, []);
+
+    const handleSendMessage = (message: string) => {
+        webSocket?.send(JSON.stringify({
+            type: "robot-agent-command",
+            question: message,
+        }));
+        setHistory(prev => [...prev, message]);
+    };
 
     return (
         <div className={styles.chatPage}>
@@ -15,7 +53,9 @@ export default function Chat() {
                 </div>
                 <div className={styles.chatMessages}>
                     <div className={styles.chatHistory}>
-                        history
+                        {history.map((message, index) => 
+                            <p key={index}>{message}</p>
+                        )}
                     </div>
                     <div className={styles.chatText}>
                         <input
@@ -27,7 +67,7 @@ export default function Chat() {
                         />
                         <button 
                             className={styles.chatButton}
-                            onClick={() => console.log("hi")}
+                            onClick={() => handleSendMessage(message)}
                         >
                             Send
                         </button>
