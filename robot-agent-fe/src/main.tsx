e5d0d25057e1b7ea48271cdedcaad04657fc40e9
Studart Erico <erico.studart@bcg.com>
2026-09-04 16:31:17 -0300

    feat: add react router

--- robot-agent-fe/src/main.tsx
+++ robot-agent-fe/src/main.tsx
@@ -1,12 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css'
-// import RobotChat from './pages/RobotChat/RobotChat'
+import RobotChat from './pages/RobotChat/RobotChat'
 import RobotCommander from './pages/RobotCommander/RobotCommander'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/*<RobotChat />*/}
-    <RobotCommander />
-  </StrictMode>,
+    <BrowserRouter>
+      <nav>
+        <Link to="/robot-commander">RobotCommander</Link> |{" "}
+        <Link to="/robot-chat">RobotChat</Link> |{" "}
+      </nav>
+      <Routes>
+        <Route path='/robot-commander' element={<RobotCommander/>}/>
+      </Routes>
+      <Routes>
+        <Route path='/robot-chat' element={<RobotChat/>}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
