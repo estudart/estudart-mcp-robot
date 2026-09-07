44cbeec3b0bb409965e4274c52d47d482ccc0315
Studart Erico <erico.studart@bcg.com>
2026-09-07 10:27:33 -0300

    fix: decrease sleep time

--- robot-mcp-server/src/application/services/distance_streamer.py
+++ robot-mcp-server/src/application/services/distance_streamer.py
@@ -35,4 +35,4 @@ class DistanceStreamer:
             except Exception as err:
                 print(f"Could not send robot distance, reason: {err}")
 
-            await asyncio.sleep(0.4)
+            await asyncio.sleep(0.1)
