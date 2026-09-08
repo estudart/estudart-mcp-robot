bb2e2798177a82314dcb17e6d42e3597074e6f62
Studart Erico <erico.studart@bcg.com>
2026-09-08 10:41:19 -0300

    feat: add servo tilt route and servo home route

--- robot-mcp-server/src/presentation/routes/servos_routes.py
+++ robot-mcp-server/src/presentation/routes/servos_routes.py
@@ -18,3 +18,34 @@ async def set_pan_angle(angle: int):
             status_code=400,
             detail=f"Could not set pan angle, reason: {err}"
         )
+
+    
+@servos_router.post("/set-tilt-angle")
+def set_tilt_angle(angle: int):
+    service = get_robot_commander()
+    try:
+        service.set_tilt_angle(angle=angle)
+        return Response(
+            content="Tilt angle updated",
+            status_code=200
+        )
+    except Exception as err:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Could not set tilt angle, reason: {err}"
+        )
+
+@servos_router.post("/servo-home")
+def servo_home():
+    service = get_robot_commander()
+    try:
+        service.servo_home()
+        return Response(
+            content="Servo angle set to home!",
+            status_code=200
+        )
+    except Exception as err:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Could not set servos to home, reason: {err}"
+        )
