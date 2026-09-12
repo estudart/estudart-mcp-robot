fd9f7f7df9a8627773eee599017563d2660f132d
Studart Erico <erico.studartdf@gmail.com>
2026-09-12 19:42:48 -0300

    fix: encode frame method to camera adapter

--- robot-mcp-server/src/application/services/camera_streamer.py
+++ robot-mcp-server/src/application/services/camera_streamer.py
@@ -2,7 +2,6 @@ import asyncio
 import json
 
 import cv2 as cv
-import base64
 
 from src.infrastructure.camera_adapter import CameraAdapter
 from src.infrastructure.web_socket_adapter import WebSocketAdapter
@@ -26,11 +25,6 @@ class CameraStreamer:
         await self._web_socket_adapter.connect()
         print("Connection stablished")
 
-    def from_frame_to_b64(self, frame) -> None:
-        _, buffer = cv.imencode('.jpg', frame)
-        b64_string = base64.b64encode(buffer).decode('utf-8')
-        return b64_string
-
     async def stream_frame(self):
         await self.connect_stream()
         print("Starting camera streaming...")
@@ -50,7 +44,9 @@ class CameraStreamer:
 
                 await self._web_socket_adapter.send_message(
                     msg_type="camera-frame",
-                    message=self.from_frame_to_b64(frame=frame)
+                    message=self._camera_adapter.from_frame_to_b64(
+                        frame=frame
+                    )
                 )
                 self._count_frame+=1
             except Exception as err:
