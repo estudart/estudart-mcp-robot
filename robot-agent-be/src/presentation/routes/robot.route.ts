3305f47473c85e84556109d795d4c892f820ce0e
Studart Erico <erico.studart@bcg.com>
2026-09-12 12:46:28 -0300

    fix: use angle inside query param

--- robot-agent-be/src/presentation/routes/robot.route.ts
+++ robot-agent-be/src/presentation/routes/robot.route.ts
@@ -45,7 +45,7 @@ robotCommanderRoutes.post('/led/setAllLeds', (req: Request, res: Response) => {
 });
 
 robotCommanderRoutes.post("/servo/setPanAngle", (req: Request, res: Response) => {
-    const { angle } = req.body;
+    const { angle } = req.query;
     const casted = Number(angle);
     if (angle !== undefined && !Number.isNaN(casted)) {
         runCommand(res, () => robotCommanderService.setPanAngle(Number(angle)));
@@ -57,7 +57,7 @@ robotCommanderRoutes.post("/servo/setPanAngle", (req: Request, res: Response) =>
 });
 
 robotCommanderRoutes.post("/servo/setTiltAngle", (req: Request, res: Response) => {
-    const { angle } = req.body;
+    const { angle } = req.query;
     const casted = Number(angle);
     if (angle !== undefined && !Number.isNaN(casted)) {
         runCommand(res, () => robotCommanderService.setTiltAngle(Number(angle)));
