7dbec5263b3acb99b18aea9e59b9ac71ebc7d637
Studart Erico <erico.studartdf@gmail.com>
2026-09-13 14:02:35 -0300

    feat: add should predict flag and pydantic settings

--- robot-mcp-server/src/application/services/camera_streamer.py
+++ robot-mcp-server/src/application/services/camera_streamer.py
@@ -1,7 +1,4 @@
 import asyncio
-import json
-
-import cv2 as cv
 
 from src.infrastructure.camera_adapter import CameraAdapter
 from src.infrastructure.web_socket_adapter import WebSocketAdapter
@@ -14,12 +11,14 @@ class CameraStreamer:
         camera_adapter: CameraAdapter,
         web_socket_adapter: WebSocketAdapter,
         image_predictor_adapter: ImagePredictorAdapter,
+        should_predict: bool,
     ) -> None:
         self._camera_adapter = camera_adapter
         self._web_socket_adapter = web_socket_adapter
         self._image_predictor_adapter = image_predictor_adapter
         self._last_result = None
         self._count_frame = 0
+        self._should_predict = should_predict
     
     async def connect_stream(self):
         await self._web_socket_adapter.connect()
@@ -32,15 +31,18 @@ class CameraStreamer:
             try:
                 frame = self._camera_adapter.get_frame()
 
-                if self._count_frame >= 5:
-                    self._last_result = (
-                        self._image_predictor_adapter
-                        .predict_image(frame=frame)
-                    )
-                    self._count_frame = 0
+                if self._should_predict:
+                    if self._count_frame >= 5:
+                        self._last_result = (
+                            self._image_predictor_adapter
+                            .predict_image(frame=frame)
+                        )
+                        self._count_frame = 0
 
-                if self._last_result:
-                    frame = self._last_result.plot(img=frame)
+                    if self._last_result:
+                        frame = self._last_result.plot(img=frame)
+                    
+                    self._count_frame+=1
 
                 await self._web_socket_adapter.send_message(
                     msg_type="camera-frame",
@@ -48,7 +50,6 @@ class CameraStreamer:
                         frame=frame
                     )
                 )
-                self._count_frame+=1
             except Exception as err:
                 print(f"Could not stream frame, reason: {err}")
                 await asyncio.sleep(10)
