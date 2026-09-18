6d894125f99e2e975f48936e52e485684690b884
Studart Erico <erico.studartdf@gmail.com>
2026-09-18 14:52:13 -0300

    fix: lock is not a callable

--- robot-mcp-server/src/infrastructure/speech_adapter.py
+++ robot-mcp-server/src/infrastructure/speech_adapter.py
@@ -1,17 +1,22 @@
 import subprocess
+from threading import Lock
 
 
 class SpeechAdapter:
+    def __init__(self):
+        self._speech_lock = Lock()
+
     def speak(self, text: str) -> None:
         text = str(text).strip()
         if not text:
             print("Did not receive a valid text")
             return
         
-        subprocess.run([
-            "espeak-ng",
-            "-d", "plughw:CARD=Device,DEV=0",
-            "-a", "200",
-            "-s", "160",
-            text
-        ])
+        with self._speech_lock:
+            subprocess.run([
+                "espeak-ng",
+                "-d", "plughw:CARD=Device,DEV=0",
+                "-a", "200",
+                "-s", "160",
+                text
+            ])
