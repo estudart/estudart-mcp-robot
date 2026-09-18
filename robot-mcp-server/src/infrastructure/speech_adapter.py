d91bd7b595eef1aea5ef2d398d6c8bfb6f979c53
Studart Erico <erico.studartdf@gmail.com>
2026-09-18 11:30:21 -0300

    feat: implement speech adapter

---
+++ robot-mcp-server/src/infrastructure/speech_adapter.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+class SpeechAdapter:
+    def speak(self, text: str) -> None:
+        text = str(text).strip()
+        if not text:
+            print("Did not receive a valid text")
+            return
+        
+        subprocess.run([
+            "espeak",
+            "-a", "200",
+            "-s", "160",
+            text
+        ])
