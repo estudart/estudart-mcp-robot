7b331abb148ac50301012c40b08ec894bd4b8822
Studart Erico <erico.studartdf@gmail.com>
2026-09-25 09:03:56 -0300

    feat: implement can store image test

--- robot-mcp-server/tests/adapters/test_camera_adapter.py
+++ robot-mcp-server/tests/adapters/test_camera_adapter.py
@@ -7,15 +7,17 @@ from src.infrastructure.camera_adapter import CameraAdapter
 def test_can_capture_frame():
     camera_adapter = CameraAdapter()
     frame = camera_adapter.get_frame()
-    assert isinstance(
-        camera_adapter.from_frame_to_bytes(frame),
-        bytes
-    )
+    from_frame_to_bytes = camera_adapter.from_frame_to_bytes(frame)
+    
+    assert isinstance(from_frame_to_bytes, bytes)
 
 def test_can_store_frame():
-    image_path = "current_frame.jpg"
     camera_adapter = CameraAdapter()
+    image_path = "current_frame.jpg"
+
     time.sleep(3)
+
     frame = camera_adapter.get_frame(path=image_path)
     image = camera_adapter.read_image(image_path)
+
     assert isinstance(image, cv2.typing.MatLike)
