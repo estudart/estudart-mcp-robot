2f9c96f205fab4f81da7ca0ad6d4eaac6c7a04a6
Studart Erico <erico.studartdf@gmail.com>
2026-09-26 13:01:53 -0300

    refactor: update to async tests

--- robot-mcp-server/tests/adapters/test_redis_adapter.py
+++ robot-mcp-server/tests/adapters/test_redis_adapter.py
@@ -1,14 +1,18 @@
 import logging
+import pytest
 
 from src.infrastructure.redis_adapter import RedisAdapter
 from src.application.services.logging_service import LoggerService
 
-def test_redis_adapter_can_set_key() -> None:
+@pytest.mark.asyncio
+async def test_redis_adapter_can_set_key() -> None:
+    logger_service = LoggerService(level=logging.INFO)
+
     key = "robot:test"
     value = 35
-    logger_service = LoggerService(level=logging.INFO)
+
     redis_adapter = RedisAdapter(logger_service)
+    await redis_adapter._create_connection()
+    await redis_adapter.set_key(key, value)
 
-    redis_adapter.set_key(key, value)
-
-    assert value == redis_adapter.get_key(key)
+    assert value == await redis_adapter.get_key(key)
